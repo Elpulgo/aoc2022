5e01fa2259526b0bab30fc17f81d0080e481da90
Oscar Larsson <oscar.larsson@positiongreen.com>
2023-12-02 12:48:18 +0100

    Day 2 2023 improvement

--- 2023/2/Day2.cs
+++ 2023/2/Day2.cs
@@ -7,6 +7,29 @@ namespace Aoc2022.Aoc2023;
 
 internal class Day2 : BaseDay
 {
+    private readonly Func<string, (int, string[], Dictionary<string, List<int>>)> _setFunc = (string line) =>
+    {
+        var lineParts = line.Split(":", StringSplitOptions.RemoveEmptyEntries);
+        var gameNr = lineParts[0].ParseSingleInt();
+        var sets = lineParts[1].Split(";");
+
+        return (gameNr, sets, new Dictionary<string, List<int>>
+        {
+            { "green", new List<int>() },
+            { "blue", new List<int>() },
+            { "red", new List<int>() }
+        });
+    };
+
+    private readonly Func<string, (int, string)> _variablesFunc = (string setPart) =>
+    {
+        var cubeInfo = setPart.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        var point = cubeInfo[0].ParseSingleInt();
+        var color = cubeInfo[1].Trim();
+
+        return (point, color);
+    };
+
     public Day2(bool shouldPrint) : base(2023, nameof(Day2), shouldPrint)
     {
     }
@@ -29,21 +52,15 @@ internal class Day2 : BaseDay
         var result = ReadInput(true)
             .Select(line =>
             {
-                var lineParts = line.Split(":", StringSplitOptions.RemoveEmptyEntries);
-                var gameNr = lineParts[0].ParseSingleInt();
-                var sets = lineParts[1].Split(";");
+                var (gameNr, sets, colorMap) = _setFunc(line);
 
-                var pointsInSetsPossible = sets
-                    .SelectMany(ss => ss.Split(",")
-                        .Select(sss =>
-                        {
-                            var cubeInfo = sss.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                            var point = cubeInfo[0].ParseSingleInt();
-                            var color = cubeInfo[1].Trim();
-                            return colorPoints.TryGetValue(color, out var maxPoint) && point <= maxPoint;
-                        }));
+                foreach (var setPart in sets.SelectMany(s => s.Split(",").Select(ss => ss)))
+                {
+                    var (point, color) = _variablesFunc(setPart);
+                    colorMap[color].Add(point);
+                }
 
-                return (gameNr, pointsInSetsPossible.All(a => a));
+                return (gameNr, colorMap.All(a => colorPoints[a.Key] >= a.Value.Max()));
             }).ToList();
 
         var sum = result.Where(w => w.Item2).Sum(s => s.gameNr);
@@ -56,33 +73,19 @@ internal class Day2 : BaseDay
         var result = ReadInput(false, partTwo: true)
             .Select(line =>
             {
-                var lineParts = line.Split(":", StringSplitOptions.RemoveEmptyEntries);
-                var sets = lineParts[1].Split(";");
-
-                var colorMap = new Dictionary<string, int>();
+                var (_, sets, colorMap) = _setFunc(line);
 
                 foreach (var setPart in sets.SelectMany(s => s.Split(",").Select(ss => ss)))
                 {
-                    var cubeInfo = setPart.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                    var point = cubeInfo[0].ParseSingleInt();
-                    var color = cubeInfo[1].Trim();
-
-                    if (colorMap.TryGetValue(color, out var maxPoint))
-                    {
-                        if (point > maxPoint)
-                        {
-                            colorMap[color] = point;
-                        }
-                    }
-                    else
-                    {
-                        colorMap[color] = point;
-                    }
+                    var (point, color) = _variablesFunc(setPart);
+                    colorMap[color].Add(point);
                 }
 
-                return colorMap.Aggregate(1, (a, b) => a * b.Value);
-            }).Sum();
+                return colorMap.Aggregate(1, (a, b) => a * b.Value.Max());
+            }).ToList();
 
-        SecondSolution(result);
+        var sum = result.Sum();
+
+        SecondSolution(sum);
     }
 }
